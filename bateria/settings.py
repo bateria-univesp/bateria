78bbf842920cc69eb55e14ee1e16b851e2877794
Lucas Viana <lv201122@gmail.com>
2022-04-21 23:40:31 -0300

    Update settings.py to allow running locally and simplify some code

--- bateria/settings.py
+++ bateria/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
-from os import path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,6 +25,7 @@ SECRET_KEY = 'django-insecure-2k&^84e5qbki@ibmvp0k6w3x(5&zeo0qc1v=udpmgw%84hw!z^
 DEBUG = True
 
 ALLOWED_HOSTS = [
+    '127.0.0.1',
     'bateria-univesp.herokuapp.com'
 ]
 
@@ -114,7 +114,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
